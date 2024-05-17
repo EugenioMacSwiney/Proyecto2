9856004c730b9f3cacf42a1e58f1d8ace5bcef71
EugenioMacSwiney <124908483+EugenioMacSwiney@users.noreply.github.com>
2024-05-17 09:43:31 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$DB_HOST=$_ENV["DB_HOST"];
-$DB_USER=$_ENV["DB_USER"];
-$DB_PASSWORD=$_ENV["DB_PASSWORD"];
-$DB_NAME=$_ENV["DB_NAME"];
-$DB_PORT=$_ENV["DB_PORT"];
+$DB_HOST=$_ENV["roundhouse.proxy.rlwy.net"];
+$DB_USER=$_ENV["root"];
+$DB_PASSWORD=$_ENV["YNCLLTwChFjiZWPrLnCcdcXLXuBhqQuM"];
+$DB_NAME=$_ENV["railway"];
+$DB_PORT=$_ENV["31415"];
 
 
-$conexion=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT")or die("error conexion");
+$conexion=mysqli_connect("$roundhouse.proxy.rlwy.net","$root","$YNCLLTwChFjiZWPrLnCcdcXLXuBhqQuM","$railway","$31415")or die("error conexion");
 ?>
 <!--  -->
